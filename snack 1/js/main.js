4bf5078a1d9d09513521aa7dc51fd965f7d0de30
amedeo <amedeopasanisi97@gmail.com>
2021-09-01 01:53:26 +0200

    partenza

---
+++ snack 1/js/main.js
@@ -0,0 +1,5 @@
+// Creare un array di oggetti:
+// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+// Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
+// BONUS: inserire una arrow function che preso in input l'array di bici ritorni l'oggetto con bici più leggera
+
