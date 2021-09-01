0cc2bdf45eae527cd4de51e0fa162bd50380065d
amedeo <amedeopasanisi97@gmail.com>
2021-09-01 18:17:11 +0200

    fine snack 1

--- snack 1/js/main.js
+++ snack 1/js/main.js
@@ -3,3 +3,24 @@
 // Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
 // BONUS: inserire una arrow function che preso in input l'array di bici ritorni l'oggetto con bici più leggera
 
+// creo un'array di oggetti bici
+const bici = [
+    {nome: "ella", peso: 11},
+    {nome: "rossella", peso: 20},
+    {nome: "mariella", peso: 11},
+    {nome: "graziella", peso: 15}
+];
+
+// ipotizzo che la prima bici sia la più leggera
+let biciLeggera = bici[0];
+// ciclo ogni elemento dell'array per vedere se ha il peso minore della bici più leggera
+for (i = 1; i < bici.length; i++) {
+    if (bici[i].peso < biciLeggera.peso)
+        biciLeggera = bici[i];
+}
+
+// destrutturo per ottenere le variabili peso e nome della bici leggera
+let {nome, peso} = biciLeggera;
+
+// uso il template literal per stampare a schermo la bici più leggera
+document.getElementById("container").innerHTML = `<h1>La bici più leggera si chiama ${nome} e pesa ${peso}Kg</h1>`
